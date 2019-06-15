6577ed3733760a3eca36819a6d6a3ba6b601c11b
Jesse <jesse.islam@gmail.com>
2019-06-15 17:05:23 -0400

    Done likelihood slide
    holds everything up to the likelihood slide, as complete

---
+++ 010-packages.R
@@ -0,0 +1,97 @@
+## ---- required-packages ----
+
+getPckg <- function(pckg) install.packages(pckg, repos = "http://cran.r-project.org")
+
+pckg = try(require(knitr))
+if(!pckg) {
+    cat("Installing 'knitr' from CRAN\n")
+    getPckg("knitr")
+    require(knitr)
+}
+
+pckg = try(require(pacman))
+if(!pckg) {
+  cat("Installing 'knitr' from CRAN\n")
+  getPckg("pacman")
+  require(pacman)
+}
+pckg = try(require(flextable))
+if(!pckg) {
+  cat("Installing 'flextable' from CRAN\n")
+  getPckg("flextable")
+  require(flextable)
+}
+pckg = try(require(casebase))
+if(!pckg) {
+  cat("Installing 'casebase' through pacman \n")
+  pacman::p_install_gh('sahirbhatnagar/casebase')
+  require(casebase)
+}
+pckg = try(require(ISLR))
+if(!pckg) {
+    cat("Installing 'ISLR' from CRAN\n")
+    getPckg("ISLR")
+    require(ISLR)
+}
+
+pckg = try(require(data.table))
+if(!pckg) {
+    cat("Installing 'data.table' from CRAN\n")
+    getPckg("data.table")
+    require(data.table)
+}
+
+
+pckg = try(require(party))
+if(!pckg) {
+    cat("Installing 'party' from CRAN\n")
+    getPckg("party")
+    require(party)
+}
+
+
+
+pckg = try(require(rpart))
+if(!pckg) {
+    cat("Installing 'rpart' from CRAN\n")
+    getPckg("rpart")
+    require(rpart)
+}
+
+pckg = try(require(rpart.plot))
+if(!pckg) {
+    cat("Installing 'rpart.plot' from CRAN\n")
+    getPckg("rpart.plot")
+    require(rpart.plot)
+}
+
+pckg = try(require(ggplot2))
+if(!pckg) {
+    cat("Installing 'ggplot2' from CRAN\n")
+    getPckg("ggplot2")
+    require(ggplot2)
+}
+
+pckg = try(require(knitcitations))
+if(!pckg) {
+    cat("Installing 'knitcitations' from CRAN\n")
+    getPckg("knitcitations")
+    require(knitcitations)
+}
+
+
+pckg = try(require(devtools))
+if(!pckg) {
+    cat("Installing 'devtools' from CRAN\n")
+    getPckg("devtools")
+    require(devtools)
+}
+
+
+pckg = try(require(DT))
+if(!pckg) {
+    cat("Installing 'DT' from Github\n")
+    devtools::install_github('rstudio/DT')
+    require(DT)
+}
+
